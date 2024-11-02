2d4e57b9a9851a1ecb39fc77d043bcf06451b4b9
Sebastiaan Booman <sebastiaan.booman@gmail.com>
2024-11-02 19:07:21 +0100

    Add keybinds for system control actions and jump list-like functionality to i3

---
+++ bin/i3/system_ctl_commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+prompt=""
+reboot=false
+
+if [[ $1 == "reboot" ]]; then
+	prompt="REBOOT? [Y/n]"
+	reboot=true
+elif [[ $1 == "poweroff" ]]; then
+	prompt="POWER OFF? [Y/n]"
+else
+	echo 'Error: must supply first argument as either "reboot" or "poweroff"'
+	exit
+fi
+
+echo $prompt
+read input
+if [[ "$input" == "y" || "$input" == "Y" || -z "$input" ]]; then
+	if [[ $reboot ]]; then
+		sudo reboot
+	else
+		sudo poweroff
+	fi
+fi
+
+# Go back to previous workspace
+/$HOME/bin/i3/workspace-history -mode=back
