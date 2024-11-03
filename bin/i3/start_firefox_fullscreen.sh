8ff0aa01d41161e865232c1e6dbe383b07a25cc6
Sebastiaan Booman <sebastiaan.booman@gmail.com>
2024-11-03 21:14:50 +0100

    improve syntax

--- bin/i3/start_firefox_fullscreen.sh
+++ bin/i3/start_firefox_fullscreen.sh
@@ -9,10 +9,7 @@ i3-msg exec firefox
 # Wait for firefox startup
 for _ in {1..5}; do
 	tree=$(i3-msg -t get_tree)
-	# Prettiest code in my life
-	firefox_started=$(echo $tree | grep -1 '"class":"firefox"')
-	firefox_started_esr=$(echo $tree | grep -1 '"class":"firefox-esr"')
-	if [[ $firefox_started || $firefox_started_esr ]]; then
+	if echo $tree | grep -q '"class":"firefox"' || echo $tree | grep -q '"class":"firefox-esr"'; then
 		# Debouncer for i3 
 		sleep 0.5
 		i3-msg fullscreen toggle
