b84eb48178904d1e1afe2d6c381626d41e1a5546
Sebastiaan Booman <74595691+SebastiaanBooman@users.noreply.github.com>
2025-02-02 13:26:28 +0100

    move lazy installation from autocommands file

--- .config/nvim/lua/lazy_init.lua
+++ .config/nvim/lua/lazy_init.lua
@@ -1,4 +1,12 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
     'git',
@@ -9,6 +17,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   }
 end
+-- Add the lazy directory path to the run time path
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
@@ -16,3 +25,6 @@ require('lazy').setup {
   import = 'plugins',
   change_detection = { notify = false },
 }
+
+--vim.opt.rtp:prepend '~/botanist.nvim'
+--vim.api.nvim_set_runtimepath '~/botanist.nvim' -- Add the local plugin to runtime path
