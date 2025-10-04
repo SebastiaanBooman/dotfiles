c820981f307428312d9dcf3f75f8495083087bb2
Sebastiaan Booman <74595691+SebastiaanBooman@users.noreply.github.com>
2025-10-04 19:57:26 +0200

    Open TODO documents upon startup

---
+++ bin/i3/open_todo_notes.sh
@@ -0,0 +1,49 @@
+# Opens a global TODO nvim file and daily TODO nvim file on i3 workspace #1
+
+# Create dirs/files if not exists
+if [ ! -d "$HOME/Documents/" ]; then 
+	mkdir "$HOME/Documents/"
+fi
+
+if [ ! -d "$HOME/Documents/notes/" ]; then
+	mkdir "$HOME/Documents/notes/"
+fi
+
+if [ ! -d "$HOME/Documents/notes/planning/" ]; then
+	mkdir "$HOME/Documents/notes/planning/"
+fi
+
+main_todo_path="$HOME/Documents/notes/planning/todo.md"
+
+if [ ! -f "$main_todo_path" ]; then
+	echo "# TODO" > "$main_todo_path"
+fi
+
+# create date variables
+printf -v year '%(%Y)T' -1
+printf -v week '%(%V)T' -1
+printf -v day '%(%d)T' -1
+
+year_path="$HOME/Documents/notes/planning/$year"
+
+if [ ! -d "$year_path" ]; then
+	mkdir "$year_path"
+fi
+
+week_path="$HOME/Documents/notes/planning/$year/$week"
+
+if [ ! -d $week_path ]; then
+	mkdir "$week_path"
+fi
+
+daily_todo_path="$HOME/Documents/notes/planning/$year/$week/planning_$day.md"
+
+if [ ! -f "$daily_todo_path" ]; then
+	echo -e "# TODO\n\n# Done" > "$daily_todo_path"
+fi
+
+i3-msg "workspace 1" > /dev/null 2>&1
+
+# because nvim can open files that dont exist yet, do not have to create a file for it
+i3-msg "exec i3-sensible-terminal -e nvim '$main_todo_path'"
+i3-msg "exec i3-sensible-terminal -e nvim '$daily_todo_path'"
